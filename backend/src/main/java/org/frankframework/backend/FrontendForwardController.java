236235a244db68065399cff687e1759e4d90ba81
matthijssmets <matthijs.smets@hotmail.com>
2025-09-01 09:51:42 +0200

    Frontend is now available from the root

--- backend/src/main/java/org/frankframework/backend/FrontendForwardController.java
+++ backend/src/main/java/org/frankframework/backend/FrontendForwardController.java
@@ -1,14 +1,14 @@
 package org.frankframework.backend;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FrontendForwardController {
-    // This is to make sure angular routing works by always going to index.html and letting angular handled the path, so refreshing on a subpath works
-    @GetMapping({"/frontend/{path:^(?!assets|api|.*\\..*$).*$}", "/frontend"})
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
     public String forward(@PathVariable String path) {
-        return "forward:/frontend/index.html";
+        return "forward:/index.html";
     }
 }
