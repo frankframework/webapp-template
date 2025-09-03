275701d65e6c338b042d5700815074e86dfb6e2c
matthijssmets <matthijs.smets@hotmail.com>
2025-09-03 10:12:44 +0200

    Put all generated cypress stuff in one folder: test-results

--- src/main/frontend/cypress.config.ts
+++ src/main/frontend/cypress.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   video: true,
   reporter: 'cypress-multi-reporters',
   reporterOptions: { configFile: 'reporter-config.json' },
+  videosFolder: './test-results/videos',
+  screenshotsFolder: './test-results/screenshot',
   e2e: {
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
