42e7bf38e4befbc64622ca089af4ce97c87b7905
Stijn Potters <93516547+stijnpotters1@users.noreply.github.com>
2025-09-25 10:33:38 +0200

    fix: changed forward to get mapping (#9)

--- src/main/java/org/frankframework/application/endpoints/FrontendForwardController.java
+++ src/main/java/org/frankframework/application/endpoints/FrontendForwardController.java
@@ -1,13 +1,13 @@
 package org.frankframework.application.endpoints;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FrontendForwardController {
 
-    @RequestMapping(value = "/{path:[^\\.]*}")
+    @GetMapping(value = "/{path:[^\\.]*}")
     public String forward(@PathVariable String path) {
         return "forward:/index.html";
     }
