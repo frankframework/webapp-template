8d84fa3b5cc9c9449f6cb1df88a4a947fa77ce8f
Vivy <4380412+Matthbo@users.noreply.github.com>
2025-10-01 09:57:42 +0200

    Backend incorrectly matched on frontend routes when hard refreshing a page in the browser (#10)

--- src/main/java/org/frankframework/application/Application.java
+++ src/main/java/org/frankframework/application/Application.java
@@ -2,6 +2,15 @@ package org.frankframework.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.function.RequestPredicate;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RequestPredicates.path;
+import static org.springframework.web.servlet.function.RequestPredicates.pathExtension;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class Application {
@@ -11,9 +20,21 @@ public class Application {
         app.run(args);
     }
 
-
-
     public static SpringApplication configureApplication() {
         return new SpringApplication(Application.class);
+    }
+
+    /**
+     * This is a custom router function to accommodate to our single page application that we serve from this spring boot backend as well.
+     * This RouterFunction will make sure that we serve `frontend/index.html` whenever the path does not start with `/api/`, is not `/error` and does
+     * not have a path extension (to exclude static resources).
+     *
+     * @see <a href="https://github.com/spring-projects/spring-framework/issues/27257">Spring framework issue 27257</a> for more details.
+     */
+    @Bean
+    RouterFunction<ServerResponse> spaRouter() {
+        ClassPathResource index = new ClassPathResource("frontend/index.html");
+        RequestPredicate spaPredicate = path("/api/**").or(path("/error")).or(pathExtension(extension -> !extension.isBlank())).negate();
+        return route().resource(spaPredicate, index).build();
     }
 }
